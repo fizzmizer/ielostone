5b656f5ac73f4fb3fe147644c85126efc29950b1
Samuel D <samrenfou@hotmail.com>
2019-04-30 18:05:03 +0200

    prepare SQL request

---
+++ php/database_id.php
@@ -0,0 +1,13 @@
+<?php
+
+// Mets ce que tu veux quand tu créeras la DB
+
+define("DB_host","localhost");
+
+define("DB_login","root");
+
+define("DB_password","1234");
+
+define("DB_database","fjc_db");
+
+?>
--- submit_page.php
+++ submit_page.php
@@ -5,48 +5,56 @@
   <body>
     <h1>Submit Match</h1>
     
-    <form method="post" action="php/submit_match.php" enctype="multipart/form-data" style="border-style:solid; border-color:white; padding:10px;">
-      <p>
-	<ins>Date:</ins> <input type="date" name="_date"/> </br></br>
-	<?php
+    <?php
+      echo '<form method="post" action="php/submit_match.php" enctype="multipart/form-data" style="border-style:solid; border-color:white; padding:10px;">';
+      echo '<p>';
+      echo '<ins>Date:</ins> <input type="date" name="_date"/> <br/><br/>';
+      
       // TODO : Gérer les nouveaux joueurs
-      $joueurs=array("Ludovic"=>"001","Antoine"=>"002","Samuel"=>"003");
-          
-          //TODO Get the array from the database
-      // require "database_id.php";
+      
+      $joueurs=array("Ludovic"=>"fjc-001","Antoine"=>"fjc-002","Samuel"=>"fjc-003");
+      // TODO Get the array from the database :
+      // require "php/database_id.php";
       // mysql_connect(DB_host,DB_login,DB_password);
       // mysql_select_db(DB_database);
-                                            
+      
+      // $sql="SELECT Nom_joueur, Matricule_joueur FROM joueurs_elo";
+      // $req=mysql_query($sql) or die('Error SQL <br/>' .$sql.'<br/>'.mysql_error());
+      
+      // while($data=mysql_fetch_assoc($req)){
+      //     $joueurs[$data["Nom_joueur"]]=$data["Matricule_joueur"];
+      // }
+      
       $nj=$_GET["nj"];
+      
+      if(!$nj){
+          $nj=2;             
+      }
+      
+      for($i=1;$i<=$nj;$i++){    
+          echo "<ins>Joueur $i :</ins><br/>";
+          echo "Nom :";
+          echo "<select name=\"mat$i\">";
+          echo "<option value=\"\"></option>";
+          foreach($joueurs as $name => $mat){
+              echo "<option value=\"$mat\">$name</option>";
+          }
+          echo "</select></br>";
+          echo "Score : <input type=\"number\" name=\"score$i\" autocomplete=\"off\" /> <br/>";
+          echo "</br>";
+      }
+      $njadd=$nj+1;
+      $njrem=$nj-1;
+      echo "<a href=\"submit_page.php?nj=$njadd\" >Add player</a></br>";
+      if($nj>2){
+          echo "<a href=\"submit_page.php?nj=$njrem\" >Remove Player</a>";
+      }
+      echo "<input type=\"hidden\" name=\"number_player\" value=\"$nj\">";
+      echo '</p>';
+      echo '<input type="submit" value="Send"/>';
+      echo '</form>';
+    ?>
 
-          if(!$nj){
-              $nj=2;             
-          }
 
-          for($i=1;$i<=$nj;$i++){    
-              echo "<ins>Joueur $i :</ins><br/>";
-              echo "Nom :";
-              echo "<select name=\"mat$i\">";
-              echo "<option value=\"\"></option>";
-              foreach($joueurs as $name => $mat){
-                  echo "<option value=\"$mat\">$name</option>";
-              }
-              echo "</select></br>";
-              echo "Score : <input type=\"number\" name=\"score$i\" autocomplete=\"off\" /> <br/>";
-              echo "</br>";
-          }
-          $njadd=$nj+1;
-          $njrem=$nj-1;
-          echo "<a href=\"submit_page.php?nj=$njadd\" >Add player</a></br>";
-          if($nj>2){
-              echo "<a href=\"submit_page.php?nj=$njrem\" >Remove Player</a>";
-          }
-          echo "<input type=\"hidden\" name=\"number_player\" value=\"$nj\">";
-			   ?>
-      </p>
-      <input type="submit" value="Send"/>
-    </form>
-    
-    
-  </body>
+</body>
 </html>
