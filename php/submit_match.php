adb6f3378abc6a6af1c57f50a714c58013947593
Samuel D <samrenfou@hotmail.com>
2019-04-30 18:38:49 +0200

    prepare sql request for submit_match

--- php/submit_match.php
+++ php/submit_match.php
@@ -1,30 +1,37 @@
-<html>
-  <head>
-    <title>Submit Match</title>
-  </head>
-  <body>
-    <h1>Submit Match</h1>
-    <?php
-      
-      // require "database_id.php";
-      // mysql_connect(DB_host,DB_login,DB_password);
-      // mysql_select_db(DB_database);
-      
-      extract($_POST);
-      
-      $matricules=array();
-      $scores=array();
-      
-      for($i=1;$i<=$number_player;$i++){
-          $matricules[]=${"mat".$i};
-          $scores[]=${"score".$i};
-      }
-      
-      for($i=0;$i<$number_player;$i++){
-          echo $matricules[$i];
-          echo $scores[$i];
-          
-      }
-      
-    ?>
-</body></html>
+<?php
+
+// require "database_id.php";
+// mysql_connect(DB_host,DB_login,DB_password);
+// mysql_select_db(DB_database);
+
+extract($_POST);
+
+$matricules=array();
+$scores=array();
+$best_score=0;
+$winner="";
+for($i=1;$i<=$number_player;$i++){
+    $matricules[]=${"mat".$i};
+    $scores[]=${"score".$i};
+    if(${"score".$i}>$best_score){
+        $best_score=${"score".$i};
+        $winner=${"mat".$i};
+    }
+}
+$joueurs_str = implode(";",$matricules);
+$scores_str = implode(";",$scores);
+
+// DEBUG
+// echo $joueurs_str;
+// echo "<br/>";
+// echo $scores_str;
+// echo "<br/>";
+// echo $best_score;
+// echo "<br/>";
+// echo $winner;
+// DEBUG
+
+// $sql="INSERT INTO match_submit (date,nombre_joueur,joueurs,scores,winner,validation) VALUES ('$date','$number_player','$joueurs_str','$scores_str','$winner',FALSE)";
+// $req = mysql_query($sql) or die('Error SQL <br/>' .$sql.'<br/>'.mysql_error());
+
+?>
