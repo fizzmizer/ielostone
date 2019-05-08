b0220c001833fe519019d829a9c51b7e5e88e908
Samuel D <samrenfou@hotmail.com>
2019-05-08 22:03:24 +0200

    Ajout d'une page pour voir l'historique des match

---
+++ pages/match_history.php
@@ -0,0 +1,33 @@
+<html>
+  <head>
+    <title>Match History</title>
+  </head>
+  <body>
+    <a href=".../index.html">Back to index</a>
+    <h1>Match History</h1>
+
+    <?php
+
+      require "../php/database_id.php";
+
+      $con = mysqli_connect(DB_host,DB_login,DB_password,DB_database);
+      $sql="SELECT Date,Dispositif,Joueurs,Scores FROM Submit WHERE Checked=TRUE";
+
+      $req = mysqli_query($con,$sql) or die('Error SQL <br/>' .$sql.'<br/>'.mysqli_error($con));
+
+      while($data=mysqli_fetch_assoc($req)){
+          echo "Date: ",$data["Date"],'<br/>';
+          echo "Dispositif: ",$data["Dispositif"],'<br/>';
+          echo "Joueurs | Scores:  <br/>";
+          $array=array_combine(explode(',',$data["Joueurs"]),explode(',',$data["Scores"]));
+          foreach($array as $joueur => $score){
+              echo '<a href="player_page.php?mat='.$joueur.'">',"fjc-00",$joueur,'</a>'," : ",$score,'<br/>';
+          }
+          echo "<br/>";
+      }
+      mysqli_close($con);
+
+    ?>    
+
+  </body>
+</html>
