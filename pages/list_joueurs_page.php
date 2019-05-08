695a44d8ba3ffee22cfcfe6289688821c1fdd92b
Samuel D <samrenfou@hotmail.com>
2019-05-08 21:11:32 +0200

    ajout de pages pour joueurs repertoriant l'historique de leurs metch et de leurs ELO

--- pages/list_joueurs_page.php
+++ pages/list_joueurs_page.php
@@ -11,12 +11,12 @@
       require "../php/database_id.php";
 
       $con = mysqli_connect(DB_host,DB_login,DB_password,DB_database);
-      $sql="SELECT Nom,Prenom,ELO FROM Joueurs WHERE Banned=FALSE AND Approved=TRUE";
+      $sql="SELECT Matricule,Nom,Prenom,ELO FROM Joueurs WHERE Banned=FALSE AND Approved=TRUE";
       
       $req = mysqli_query($con,$sql) or die('Error SQL <br/>' .$sql.'<br/>'.mysqli_error($con));
 
       while($data=mysqli_fetch_assoc($req)){
-          echo $data["Prenom"]," ",$data["Nom"]," : ",$data["ELO"];
+          echo '<a href="player_page.php?mat='.$data['Matricule'].'">',$data["Prenom"]," ",$data["Nom"]," : ",$data["ELO"],"</a>";
           echo '<br/>';
       }
       mysqli_close($con);
